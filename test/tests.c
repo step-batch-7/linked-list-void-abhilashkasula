a60133d53e4875b543dd927364e748081d7f8343
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-05-13 19:24:36 +0530

    added tests for add_to_start for numbers

--- test/tests.c
+++ test/tests.c
@@ -27,6 +27,8 @@ Element create_char(char value)
 
 void test_add_to_list(void)
 {
+  printf("add_to_list\n");
+
   List_ptr list_1 = create_list();
   Element number_1_1 = create_int(5);
   Element expected_1[1];
@@ -74,9 +76,48 @@ void test_add_to_list(void)
   destroy_list(list_4);
 }
 
+void test_add_to_start(void)
+{
+  printf("add_to_start\n");
+
+  List_ptr list_1 = create_list();
+  Element number_1_1 = create_int(4);
+  Element expected_1[1];
+  expected_1[0] = number_1_1;
+  add_to_start(list_1, number_1_1);
+  char message_1[] = "should add number to start when the list is empty";
+  display_assertion(assert_list(list_1, expected_1, 1, &assert_int), message_1);
+
+  List_ptr list_2 = create_list();
+  Element number_2_1 = create_int(4);
+  Element number_2_2 = create_int(5);
+  Element expected_2[2];
+  expected_2[0] = number_2_2;
+  expected_2[1] = number_2_1;
+  add_to_start(list_2, number_2_1);
+  add_to_start(list_2, number_2_2);
+  char message_2[] = "should add number to start when the list has one element";
+  display_assertion(assert_list(list_2, expected_2, 2, &assert_int), message_2);
+
+  List_ptr list_3 = create_list();
+  Element number_3_1 = create_int(4);
+  Element number_3_2 = create_int(5);
+  Element number_3_3 = create_int(6);
+  Element expected_3[3];
+  expected_3[0] = number_3_3;
+  expected_3[1] = number_3_2;
+  expected_3[2] = number_3_1;
+  add_to_start(list_3, number_3_1);
+  add_to_start(list_3, number_3_2);
+  add_to_start(list_3, number_3_3);
+  char message_3[] = "should add number to start when the list has more than one element";
+  display_assertion(assert_list(list_3, expected_3, 3, &assert_int), message_3);
+}
+
 int main(void)
 {
   test_add_to_list();
+  test_add_to_start();
   display_passing_count();
   return 0;
 }
