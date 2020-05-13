9d7ffc985af020fb5ad3864b6251848925eb175e
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-05-13 18:56:45 +0530

    added tests for add_to_list numbers

--- test/tests.c
+++ test/tests.c
@@ -14,14 +14,28 @@ Element create_int(int value)
 
 void test_add_to_list(void)
 {
-  List_ptr list = create_list();
+  List_ptr list_1 = create_list();
   Element number_1_1 = create_int(5);
-  Element elements_1[1];
-  elements_1[0] = number_1_1;
-  add_to_list(list, number_1_1);
-  char message_1[] = "should add an element to the list when the list is empty";
-  display_assertion(assert_list(list, elements_1, 1, &assert_int), message_1);
-  destroy_list(list);
+  Element expected_1[1];
+  expected_1[0] = number_1_1;
+  add_to_list(list_1, number_1_1);
+  char message_1[] = "should add number to the list when the list is empty";
+  display_assertion(assert_list(list_1, expected_1, 1, &assert_int), message_1);
+  destroy_list(list_1);
+
+  List_ptr list_2 = create_list();
+  Element number_2_1 = create_int(4);
+  Element number_2_2 = create_int(5);
+  Element number_2_3 = create_int(6);
+  Element expected_2[3];
+  expected_2[0] = number_2_1;
+  expected_2[1] = number_2_2;
+  expected_2[2] = number_2_3;
+  add_to_list(list_2, number_2_1);
+  add_to_list(list_2, number_2_2);
+  add_to_list(list_2, number_2_3);
+  char message_2[] = "should add number to the list when the list has elements";
+  display_assertion(assert_list(list_2, expected_2, 3, &assert_int), message_2);
 }
 
 int main(void)
