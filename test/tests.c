1c9cb5ef4560a3dfa1f1106806ab5e3e961a74e9
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-05-13 19:57:07 +0530

    added tests for reverse function

--- test/tests.c
+++ test/tests.c
@@ -87,6 +87,7 @@ void test_add_to_start(void)
   add_to_start(list_1, number_1_1);
   char message_1[] = "should add number to start when the list is empty";
   display_assertion(assert_list(list_1, expected_1, 1, &assert_int), message_1);
+  destroy_list(list_1);
 
   List_ptr list_2 = create_list();
   Element number_2_1 = create_int(4);
@@ -98,6 +99,7 @@ void test_add_to_start(void)
   add_to_start(list_2, number_2_2);
   char message_2[] = "should add number to start when the list has one element";
   display_assertion(assert_list(list_2, expected_2, 2, &assert_int), message_2);
+  destroy_list(list_2);
 
   List_ptr list_3 = create_list();
   Element number_3_1 = create_int(4);
@@ -112,6 +114,7 @@ void test_add_to_start(void)
   add_to_start(list_3, number_3_3);
   char message_3[] = "should add number to start when the list has more than one element";
   display_assertion(assert_list(list_3, expected_3, 3, &assert_int), message_3);
+  destroy_list(list_3);
 
   List_ptr list_4 = create_list();
   Element letter_4_1 = create_char('a');
@@ -120,6 +123,7 @@ void test_add_to_start(void)
   add_to_start(list_4, letter_4_1);
   char message_4[] = "should add a character to start when the list is empty";
   display_assertion(assert_list(list_4, expected_4, 1, &assert_char), message_4);
+  destroy_list(list_4);
 
   List_ptr list_5 = create_list();
   Element letter_5_1 = create_char('a');
@@ -134,12 +138,52 @@ void test_add_to_start(void)
   add_to_start(list_5, letter_5_3);
   char message_5[] = "should add character to start when the list has more than one element";
   display_assertion(assert_list(list_5, expected_5, 3, &assert_char), message_5);
+  destroy_list(list_5);
+}
+
+void test_reverse(void)
+{
+  printf("reverse\n");
+
+  List_ptr numbers_1 = create_list();
+  List_ptr actual_1 = reverse(numbers_1);
+  Element expected_1[0];
+  char message_1[] = "should give empty list for empty list given";
+  display_assertion(assert_list(actual_1, expected_1, 0, &assert_int), message_1);
+  destroy_list(numbers_1);
+
+  List_ptr numbers_2 = create_list();
+  Element number_2_1 = create_int(5);
+  Element expected_2[1];
+  expected_2[0] = number_2_1;
+  add_to_list(numbers_2, number_2_1);
+  List_ptr actual_2 = reverse(numbers_2);
+  char message_2[] = "should give the same list for given list contains only one element";
+  display_assertion(assert_list(actual_2, expected_2, 1, &assert_int), message_2);
+  destroy_list(numbers_2);
+
+  List_ptr numbers_3 = create_list();
+  Element number_3_1 = create_int(5);
+  Element number_3_2 = create_int(6);
+  Element number_3_3 = create_int(7);
+  Element expected_3[3];
+  expected_3[0] = number_3_3;
+  expected_3[1] = number_3_2;
+  expected_3[2] = number_3_1;
+  add_to_list(numbers_3, number_3_1);
+  add_to_list(numbers_3, number_3_2);
+  add_to_list(numbers_3, number_3_3);
+  List_ptr actual_3 = reverse(numbers_3);
+  char message_3[] = "should reverse the numbers for more than one element given";
+  display_assertion(assert_list(actual_3, expected_3, 3, &assert_int), message_3);
+  destroy_list(numbers_3);
 }
 
 int main(void)
 {
   test_add_to_list();
   test_add_to_start();
+  test_reverse();
   display_passing_count();
   return 0;
 }
